f4c9b9ac3199818c5182e20653bf9276639cacc9
Ítalo Nolasco <italonolascoramos@gmail.com>
2020-04-28 19:37:50 -0300

    Redux com carrinho ok. Reactotron integrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -12,8 +14,22 @@ export default class Home extends Component {
   async componentDidMount() {
     const response = await api.get('products');
 
-    this.setState({ products: response.data });
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
   }
+
+  handleAddProduct = (product) => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
 
   render() {
     const { products } = this.state;
@@ -24,9 +40,12 @@ export default class Home extends Component {
           <li key={product.id}>
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
-            <span>{product.price}</span>
+            <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -39,3 +58,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
