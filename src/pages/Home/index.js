f1bd2b7a9a9dc9d4fd09cec9a920295c3d5fbe9b
Ítalo Nolasco <italonolascoramos@gmail.com>
2020-04-28 03:58:32 -0300

    Design feito

---
+++ src/pages/Home/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-chuck-taylor-all-star-platform-hi/16/D26-0258-016/D26-0258-016_zoom1.jpg?ts=1587445391&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Tênis bem gay</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
